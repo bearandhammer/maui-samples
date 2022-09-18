7171cb7dbcc1821b36c301b6f0f958d0b4fd8abd
Lewis Grint <lgrint1@yahoo.co.uk>
2022-09-18 13:55:45 +0100

    Change to AboutPage to use data binding.

--- NoteTaker/Maui.NoteTaker.Sample/Maui.NoteTaker.Sample/Views/AboutPage.xaml.cs
+++ NoteTaker/Maui.NoteTaker.Sample/Maui.NoteTaker.Sample/Views/AboutPage.xaml.cs
@@ -1,12 +1,19 @@
+using Maui.NoteTaker.Sample.Models;
+
 namespace Maui.NoteTaker.Sample.Views;
 
 public partial class AboutPage : ContentPage
 {
-	public AboutPage()
-	{
-		InitializeComponent();
-	}
+    public AboutPage()
+    {
+        InitializeComponent();
+    }
 
-	private async void LearnMore_Clicked(object sender, EventArgs e) => 
-		await Launcher.Default.OpenAsync("https://aka.ms/maui");
+    private async void LearnMore_Clicked(object sender, EventArgs e)
+    {
+        if (BindingContext is About aboutModel)
+        {
+            await Launcher.Default.OpenAsync(aboutModel.MoreInfoUrl);
+        }
+    }
 }
