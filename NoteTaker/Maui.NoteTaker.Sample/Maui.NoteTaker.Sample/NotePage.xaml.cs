49dd73660e6329e727be6b6793559075590f6006
Lewis Grint <lgrint1@yahoo.co.uk>
2022-09-18 13:21:50 +0100

    Basic stubbing for NotePage (as per sample).

--- NoteTaker/Maui.NoteTaker.Sample/Maui.NoteTaker.Sample/NotePage.xaml.cs
+++ NoteTaker/Maui.NoteTaker.Sample/Maui.NoteTaker.Sample/NotePage.xaml.cs
@@ -2,8 +2,32 @@ namespace Maui.NoteTaker.Sample;
 
 public partial class NotePage : ContentPage
 {
-	public NotePage()
-	{
-		InitializeComponent();
-	}
+    private readonly string fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
+
+    public NotePage()
+    {
+        InitializeComponent();
+
+        if (File.Exists(fileName))
+        {
+            TextEditor.Text = File.ReadAllText(fileName);
+        }
+    }
+
+    private void DeleteButton_Clicked(object sender, EventArgs e)
+    {
+        // Delete the file.
+        if (File.Exists(fileName))
+        {
+            File.Delete(fileName);
+        }
+
+        TextEditor.Text = string.Empty;
+    }
+
+    private void SaveButton_Clicked(object sender, EventArgs e)
+    {
+        // Save the file.
+        File.WriteAllText(fileName, TextEditor.Text);
+    }
 }
