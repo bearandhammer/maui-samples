8a69c1fbb29006109953af4d8064f95d5d02bba2
Lewis Grint <lgrint1@yahoo.co.uk>
2022-09-18 14:24:00 +0100

    Register NotePage for navigation.
    Now it is not automatically registered via the TabBar.

--- NoteTaker/Maui.NoteTaker.Sample/Maui.NoteTaker.Sample/AppShell.xaml.cs
+++ NoteTaker/Maui.NoteTaker.Sample/Maui.NoteTaker.Sample/AppShell.xaml.cs
@@ -1,9 +1,13 @@
-﻿namespace Maui.NoteTaker.Sample;
+﻿using Maui.NoteTaker.Sample.Views;
+
+namespace Maui.NoteTaker.Sample;
 
 public partial class AppShell : Shell
 {
 	public AppShell()
 	{
 		InitializeComponent();
+
+		Routing.RegisterRoute(nameof(NotePage), typeof(NotePage));
 	}
 }
