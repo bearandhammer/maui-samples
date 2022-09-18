2235b48ff5c801a1776f59efbb68cbbb75189a44
Lewis Grint <lgrint1@yahoo.co.uk>
2022-09-18 13:42:17 +0100

    Add basic models for Note and About.

---
+++ NoteTaker/Maui.NoteTaker.Sample/Maui.NoteTaker.Sample/Models/About.cs
@@ -0,0 +1,10 @@
+﻿namespace Maui.NoteTaker.Sample.Models
+{
+    internal class About
+    {
+        public string Message => "This app is written in XAML and C# with .NET MAUI.";
+        public string MoreInfoUrl => "https://aka.ms/maui";
+        public string Title => AppInfo.Name;
+        public string Version => AppInfo.VersionString;
+    }
+}
