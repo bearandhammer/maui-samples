f4e167958492a57e3180e472b1c287c60dba5f66
Lewis Grint <lgrint1@yahoo.co.uk>
2022-09-18 12:41:56 +0100

    Initial commit for Maui.NoteTaker.Sample.

---
+++ NoteTaker/Maui.NoteTaker.Sample/Maui.NoteTaker.Sample/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Maui.NoteTaker.Sample;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ NoteTaker/Maui.NoteTaker.Sample/Maui.NoteTaker.Sample/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Maui.NoteTaker.Sample;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
