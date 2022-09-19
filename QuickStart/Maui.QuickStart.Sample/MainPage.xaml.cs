6b74574f032bc25674ecc1c5d2f3951afc8edea7
Lewis Grint <lgrint1@yahoo.co.uk>
2022-09-19 13:34:28 +0100

    Introduction of some basic notes/comments.

--- QuickStart/Maui.QuickStart.Sample/MainPage.xaml.cs
+++ QuickStart/Maui.QuickStart.Sample/MainPage.xaml.cs
@@ -1,30 +1,49 @@
 ﻿namespace Maui.QuickStart.Sample;
 
+/// <summary>
+/// References the main content page for this application.
+/// </summary>
+/// <seealso cref="ContentPage" />
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    /// <summary>
+    /// Tracking count representing the amount of times the 'counter' button is clicked.
+    /// </summary>
+    private int count = 0;
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MainPage"/> class.
+    /// </summary>
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    /// <summary>
+    /// Event method called when the 'CounterBtn' is clicked.
+    /// </summary>
+    /// <param name="sender">The sender object triggering this event.</param>
+    /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+        CounterBtn.Text = count == 1
+            ? $"Clicked {count} time"
+            : $"Clicked {count} times";
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
 
-	private void OnMultiplesBtnClicked(object sender, EventArgs e)
-	{
-		MultiplesBtn.Text = $"Clicked x2 equals {count * 2}";
+    /// <summary>
+    /// Event method called when the 'CounterBtn' is clicked.
+    /// </summary>
+    /// <param name="sender">The sender object triggering this event.</param>
+    /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+    private void OnMultiplesBtnClicked(object sender, EventArgs e)
+    {
+        MultiplesBtn.Text = $"Clicked x2 equals {count * 2}";
 
-		SemanticScreenReader.Announce(MultiplesBtn.Text);
-	}
+        SemanticScreenReader.Announce(MultiplesBtn.Text);
+    }
 }
