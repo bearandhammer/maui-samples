d413212043831adf5de9d39246ee46dc07af1a74
Lewis Grint <lgrint1@yahoo.co.uk>
2022-09-19 13:49:27 +0100

    Tidying models.

--- NoteTaker/Maui.NoteTaker.Sample/Maui.NoteTaker.Sample/Models/Note.cs
+++ NoteTaker/Maui.NoteTaker.Sample/Maui.NoteTaker.Sample/Models/Note.cs
@@ -1,9 +1,24 @@
 ﻿namespace Maui.NoteTaker.Sample.Models
 {
+    /// <summary>
+    /// Internal class acting as a model for the AllNotesPage.
+    /// </summary>
     internal class Note
     {
-        public DateTime Date { get; set; }
-        public string Filename { get; set; }
-        public string Text { get; set; }
+        /// <summary>
+        /// Gets or sets the date the note was saved.
+        /// </summary>
+        internal DateTime Date { get; set; }
+
+        /// <summary>
+        /// Gets or sets the filename of the note.
+        /// </summary>
+        /// <value>
+        internal string Filename { get; set; }
+
+        /// <summary>
+        /// Gets or sets the text of the note.
+        /// </summary>
+        internal string Text { get; set; }
     }
 }
