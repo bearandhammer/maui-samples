9bbe43d48c900cd730fd11e6f5db9b3854170be1
Lewis Grint <lgrint1@yahoo.co.uk>
2022-09-19 14:16:22 +0100

    Tidying up of page code.

--- NoteTaker/Maui.NoteTaker.Sample/Maui.NoteTaker.Sample/Views/NotePage.xaml.cs
+++ NoteTaker/Maui.NoteTaker.Sample/Maui.NoteTaker.Sample/Views/NotePage.xaml.cs
@@ -2,32 +2,46 @@ using Maui.NoteTaker.Sample.Models;
 
 namespace Maui.NoteTaker.Sample.Views;
 
+/// <summary>
+/// Represents the NotePage for the NoteTaker application. Note the <see cref="ItemId"/> that
+/// is configured as a navigation <see cref="QueryPropertyAttribute"/>.
+/// </summary>
+/// <seealso cref="ContentPage" />
 [QueryProperty(nameof(ItemId), nameof(ItemId))]
 public partial class NotePage : ContentPage
 {
-    private readonly string fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
-
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NotePage"/> class.
+    /// </summary>
     public NotePage()
     {
         InitializeComponent();
 
-        string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-        string randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
+        string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+            randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
 
         LoadNote(Path.Combine(appDataPath, randomFileName));
     }
 
+    /// <summary>
+    /// Sets the ItemId representing the note to be loaded.
+    /// </summary>
     public string ItemId
     {
-        set 
-        { 
-            LoadNote(value); 
+        set
+        {
+            LoadNote(value);
         }
     }
 
+    /// <summary>
+    /// Handles the Clicked event of the DeleteButton control.
+    /// </summary>
+    /// <param name="sender">The source of the event.</param>
+    /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
     private async void DeleteButton_Clicked(object sender, EventArgs e)
     {
-        if (BindingContext is Models.Note note)
+        if (BindingContext is Note note)
         {
             // Delete the file.
             if (File.Exists(note.Filename))
@@ -39,9 +53,14 @@ public partial class NotePage : ContentPage
         await Shell.Current.GoToAsync("..");
     }
 
+    /// <summary>
+    /// Handles the Clicked event of the SaveButton control.
+    /// </summary>
+    /// <param name="sender">The source of the event.</param>
+    /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
     private async void SaveButton_Clicked(object sender, EventArgs e)
     {
-        if (BindingContext is Models.Note note)
+        if (BindingContext is Note note)
         {
             File.WriteAllText(note.Filename, TextEditor.Text);
         }
@@ -49,6 +68,10 @@ public partial class NotePage : ContentPage
         await Shell.Current.GoToAsync("..");
     }
 
+    /// <summary>
+    /// Loads an individual note based on the supplied file name.
+    /// </summary>
+    /// <param name="fileName">The file name of the note to load.</param>
     private void LoadNote(string fileName)
     {
         Note noteModel = new Note
